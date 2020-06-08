902d5850c9c461be21d1192917df8b830acd7752
daxupaxu <dawid.daoxuan@gmail.com>
2020-06-08 20:09:12 +0100

    updated webpack + new font

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3845050d8b398339f43f";
+/******/ 	var hotCurrentHash = "70e0c0fc0aa66cb76f34";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
